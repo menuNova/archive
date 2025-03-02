9c8166185fa859af97b802194a92414778a40299
thisSasha <thisDevSasha@gmail.com>
2025-03-02 19:23:31 +0300

    1.87

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 let table = new URLSearchParams(window.location.search).get('table');
 if (table) {
-    sessionStorage.setItem('table', table)
+    sessionStorage.setItem(textToId(data.name) + '-table', table)
 };
 
 for (let i = 0; i < indexData.length; i++) {
