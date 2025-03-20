15ce560c213c9841df89896c957732057c4f3727
thisSasha <thisDevSasha@gmail.com>
2025-03-20 21:11:01 +0300

    2.01

--- js/globalStore.js
+++ js/globalStore.js
@@ -114,39 +114,3 @@ let languageData = {
         '#payOrder': 'Zahlung machen',
     },
 };
-
-function textToId(text) {
-    return text
-        .normalize('NFKD') 
-        .replace(/[\u0400-\u04FF\u0500-\u052F\u1E00-\u1EFF]/g, match => {
-            return match.charCodeAt(0).toString(36);
-        })
-        .replace(/[^a-zA-Z0-9-]/g, '-')
-        .replace(/-+/g, '-')
-        .replace(/^-+|-+$/g, '')
-        .toLowerCase();
-};
-
-function setCartData(newData) {
-    cartData = newData;
-};
-
-let setDishesData = function (newData) {
-    dishesData = newData;
-};
-
-let getDishesData = function () {
-    return dishesData;
-};
-
-let setLastOrder = function (newLastOrder, isNew = true) {
-    lastOrder = newLastOrder;
-    if (newLastOrder != 'delete') {
-        allLasts.push(lastOrder);
-    } else {
-        allLasts = JSON.parse(localStorage.getItem(textToId(data.name) + '-allLastS'));
-        lastOrder = allLasts.pop();
-    };
-    localStorage.setItem(textToId(data.name) + '-allLastS', JSON.stringify(allLasts))
-    localStorage.setItem(textToId(data.name) + '-lastS', JSON.stringify(lastOrder));
-};
